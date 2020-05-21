40106d844c01d1a4b267f703210ff26d579afeba
Maartje Eyskens <maartje@eyskens.me>
2020-05-21 16:04:27 +0200

    fck srcd
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- cmd/repo-create/clone.go
+++ cmd/repo-create/clone.go
@@ -6,11 +6,11 @@ import (
 	"log"
 	"os"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/config"
-	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
 )
 
 func init() {
